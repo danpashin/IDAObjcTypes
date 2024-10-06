f1a1309dfcd157b8833aa019362f806ad064749e
Dan Pashin <danypashin@yandex.ru>
2024-10-06 23:27:01 +0300

    Add more signatures to libmis

--- libmis/Types.h
+++ libmis/Types.h
@@ -9,7 +9,7 @@ typedef const struct _MISBlacklistData *MISBlacklistDataRef;
 #define sub_mobiledevice_mis (0x02 << 14)
 #define MISMakeError(err) (sys_mobiledevice | sub_mobiledevice_mis | err)
 
-enum {
+PS_ENUM(int64_t, MISError) {
     kMISNoError = 0,
     kMISUnknownError = MISMakeError(0x01),
     kMISNotMutableError = MISMakeError(0x02),
@@ -39,9 +39,22 @@ enum {
     kMISProfileSignatureNotValidError = MISMakeError(0x1A),
     kMISDeviceCommunicationError = MISMakeError(0x1B),
     kMISCodeNotSignedError = MISMakeError(0x1C),
-    kMISMaximumErrorValue,
+    kMISRejectedByPolicyError = MISMakeError(0x1D),
+    kMISNonexistentProfileError = MISMakeError(0x1E),
+    kMISUnentitledBetaProfileError = MISMakeError(0x1F),
+    kMISUnentitledBetaProfileOverLockdownError = MISMakeError(0x20),
+    kMISFreeProfileMaximumAppsExceededError = MISMakeError(0x21),
+    kMISProfileDatabaseAccessError = MISMakeError(0x22),
+    kMISAgentCommunicationError = MISMakeError(0x23),
+    kMISBannedProfileError = MISMakeError(0x24),
+    kMISUntrustedProfileError = MISMakeError(0x25),
+    kMISProfileRequiresOnlineError = MISMakeError(0x26),
+    kMISCDHashNotInTrustCacheError = MISMakeError(0x27),
+    kMISInvalidArgumentError = MISMakeError(0x28),
+    kMISUnsupportedCodeSignatureVersionError = MISMakeError(0x29),
+    kMISMissingProfisionProfileError = MISMakeError(0x2A),
+    kMISDeveloperModeRequiredError = MISMakeError(0x2B),
 };
-typedef int MISError;
 
 
 #endif // LIBMIS_H_
--- libmis/libmis.h
+++ libmis/libmis.h
@@ -2,12 +2,30 @@
 #import "Types.h"
 #import "../Foundation/Foundation.h"
 
-CFStringRef MISCopyErrorStringForErrorCode(MISError error);
+// Funcs with unknown signatures
+// amfi_interface_cdhash_in_trustcache
+// amfi_interface_query_bootarg_state
+// amfi_interface_get_local_signing_private_key
+// amfi_interface_get_local_signing_public_key
+// amfi_interface_set_local_signing_public_key
+// amfi_interface_authorize_local_signing
+// amfi_launch_constraint_set_spawnattr
 
-CFTypeID MISProfileGetTypeID(void);
+//
+// - AMFI part
+//
+bool amfi_developer_mode_status(void);
+bool amfi_developer_mode_resolved(void);
 
+
+//
+// - Profile
+//
+Boolean MISProfileIsDEREncoded(MISProfileRef profile);
+Boolean MISProfileIsMutable(MISProfileRef profile);
 CFDataRef MISProfileCreateDataRepresentation(MISProfileRef profile);
 CFDictionaryRef MISProfileCopyPayload(MISProfileRef profile);
+CFTypeID MISProfileGetTypeID(void);
 CFTypeRef MISProfileGetValue(MISProfileRef profile, CFStringRef key);
 MISError MISProfileCopySignerSubjectSummary(MISProfileRef profile, CFStringRef *summary);
 MISError MISProfileRemoveValue(MISMutableProfileRef profile, CFStringRef key);
@@ -23,30 +41,75 @@ MISMutableProfileRef MISProfileCreate(CFAllocatorRef allocator);
 MISMutableProfileRef MISProfileCreateMutableCopy(CFAllocatorRef allocator, MISProfileRef profile);
 MISProfileRef MISProfileCreateWithData(CFAllocatorRef allocator, CFDataRef data);
 MISProfileRef MISProfileCreateWithFile(CFAllocatorRef allocator, CFStringRef file);
-bool MISProfileIsMutable(MISProfileRef profile);
 
-bool MISExistsIndeterminateApps(void);
 
+//
+// - ProvisioningProfile
+//
+Boolean MISProvisioningProfileGrantsEntitlement(MISProfileRef profile, CFStringRef entitlement, CFTypeRef value);
+Boolean MISProvisioningProfileHasPPQRequirement(MISProfileRef profile);
+Boolean MISProvisioningProfileIncludesDevice(MISProfileRef profile, CFStringRef deviceID);
+Boolean MISProvisioningProfileIncludesPlatform(MISProfileRef profile, CFStringRef platformName);
+Boolean MISProvisioningProfileIsAppleInternalProfile(MISProfileRef profile);
+Boolean MISProvisioningProfileIsForBetaDeploymen(MISProfileRef profile);
+Boolean MISProvisioningProfileIsForBetaDeployment(MISProfileRef profile);
+Boolean MISProvisioningProfileIsForLocalProvisioning(MISProfileRef profile);
+Boolean MISProvisioningProfileProvisionsAllDevices(MISProfileRef profile);
 CFArrayRef MISProvisioningProfileCopyApplicationIdentifierPrefixes(MISProfileRef profile);
+CFArrayRef MISProvisioningProfileGetDeveloperCertificates(MISProfileRef profile);
+CFArrayRef MISProvisioningProfileGetDeveloperCertificatesHashes(MISProfileRef profile);
+CFArrayRef MISProvisioningProfileGetProvisionedDevices(MISProfileRef profile);
+CFArrayRef MISProvisioningProfileGetProvisioningCDHashes(MISProfileRef profile);
+CFDateRef MISProvisioningProfileGetCreationDate(MISProfileRef profile);
+CFDateRef MISProvisioningProfileGetExpirationDate(MISProfileRef profile);
+CFDictionaryRef MISProvisioningProfileGetEntitlements(MISProfileRef profile);
 CFStringRef MISProvisioningProfileGetName(MISProfileRef profile);
 CFStringRef MISProvisioningProfileGetTeamIdentifier(MISProfileRef profile);
-CFTypeRef MISProvisioningProfileGetCreationDate(MISProfileRef profile);
-CFTypeRef MISProvisioningProfileGetDeveloperCertificates(MISProfileRef profile);
-CFTypeRef MISProvisioningProfileGetDeveloperCertificatesHashes(MISProfileRef profile);
-CFTypeRef MISProvisioningProfileGetExpirationDate(MISProfileRef profile);
-CFTypeRef MISProvisioningProfileGetProvisionedDevices(MISProfileRef profile);
+CFStringRef MISProvisioningProfileGetUUID(MISProfileRef profile);
+CFTypeRef MISProvisioningProfileGetEmbeddedDER(MISProfileRef profile);
 CFTypeRef MISProvisioningProfileGetRawEntitlements(MISProfileRef profile);
-CFTypeRef MISProvisioningProfileGetUUID(MISProfileRef profile);
-NSInteger MISProvisioningProfileGetVersion(MISProfileRef profile);
-bool MISProvisioningProfileGrantsEntitlement(MISProfileRef profile, CFStringRef entitlement);
-bool MISProvisioningProfileHasPPQRequirement(MISProfileRef profile);
-bool MISProvisioningProfileIncludesPlatform(MISProfileRef profile, CFStringRef platformName);
-bool MISProvisioningProfileIsAppleInternalProfile(MISProfileRef profile);
-bool MISProvisioningProfileIsForLocalProvisioning(MISProfileRef profile);
-bool MISProvisioningProfileProvisionsAllDevices(MISProfileRef profile);
+MISError MISProvisioningProfileAddDeveloperCertificate(MISMutableProfileRef profile, CFDataRef cert);
+MISError MISProvisioningProfileAddEntitlement(MISMutableProfileRef profile, CFStringRef entitlement, CFTypeRef value);
+MISError MISProvisioningProfileAddProvisionedDevice(MISMutableProfileRef profile, CFStringRef deviceID);
+MISError MISProvisioningProfileCheckValidity(MISProfileRef profile, CFStringRef deviceID, CFDateRef date);
+MISError MISProvisioningProfileSetApplicationIdentifierPrefixes(MISMutableProfileRef profile, CFArrayRef prefixes);
+MISError MISProvisioningProfileSetDeveloperCertificates(MISMutableProfileRef profile, CFArrayRef certs);
+MISError MISProvisioningProfileSetName(MISMutableProfileRef profile, CFStringRef name);
+MISError MISProvisioningProfileSetProvisionsAllDevices(MISMutableProfileRef profile);
+MISError MISProvisioningProfileSetTimeToLive(MISMutableProfileRef profile, int days);
+MISError MISProvisioningProfileValidateSignature(MISProfileRef profile);
+MISMutableProfileRef MISProfileCreateForProvisioning(CFAllocatorRef allocator);
+MISMutableProfileRef MISProvisioningProfileCreateMutableCopy(CFAllocatorRef alloc, MISProfileRef profile);
+int MISProvisioningProfileGetVersion(MISProfileRef profile);
 
-MISError MISQueryBlacklistForBundle(CFStringRef path, bool flag, bool overridenFlag, void **result, MISBlacklistDataRef *blacklist, NSUInteger *haty);
-MISError MISQueryBlacklistForCdHash(CFDataRef, bool flag, bool overridenFlag, void **result);
 
-MISError MISBlacklistSetOverride(MISBlacklistDataRef blacklist, NSUInteger haty, bool override);
-
+//
+// - Generic functions
+//
+Boolean MISAppApprovalState(CFStringRef path, CFDictionaryRef options);
+Boolean MISArrayAllowsEntitlementValue(CFArrayRef entitlements, CFStringRef value);
+Boolean MISBlacklistOverriddenByUser(CFDataRef blacklistData, int someOptions);
+Boolean MISEntitlementDictionaryAllowsEntitlementValue(CFDictionaryRef entitlements, CFStringRef entitlement, CFTypeRef value);
+Boolean MISExistsIndeterminateApps(CFDataRef data, CFTypeRef arg2, Boolean *result);
+Boolean MISUPPTrusted(CFStringRef upp);
+CFStringRef MISCopyErrorStringForErrorCode(MISError error);
+CFStringRef MISCopyErrorStringForErrorCodeUnlocalized(MISError error);
+CFTypeRef CopyMockUDID(void);
+MISError MISBlacklistSetOverride(MISBlacklistDataRef blacklist, NSUInteger haty, Boolean override);
+MISError MISCopyInstalledProvisioningProfiles(CFArrayRef *profiles);
+MISError MISCopyProvisioningProfile(CFStringRef profileName, MISProfileRef *profile);
+MISError MISCopyProvisioningProfileWithConnection(CFStringRef profileName, MISProfileRef *profile, xpc_connection_t connection);
+MISError MISEnumerateInstalledProvisioningProfiles(int flags, void *enumBlock);
+MISError MISEnumerateMatchingProfiles(CFDataRef certificate, CFArrayRef predicates, void *enumBlock);
+MISError MISEnumerateMatchingProfilesUnauthoritative(CFDataRef certificate);
+MISError MISInstallProvisioningProfile(MISProfileRef profile);
+MISError MISPing(char **reply);
+MISError MISQueryBlacklistForBundle(CFStringRef path, Boolean flag, Boolean overridenFlag, void **result, MISBlacklistDataRef *blacklist, NSUInteger *haty);
+MISError MISQueryBlacklistForCdHash(CFDataRef someData, int someOptions, Boolean overridenFlag, void **result);
+MISError MISRemoveProvisioningProfile(MISProfileRef profile);
+MISError MISValidateSignature(CFStringRef path, CFDictionaryRef options);
+MISError MISValidateSignatureAndCopyInfo(CFStringRef path, CFDictionaryRef options, CFDictionaryRef *info);
+MISError MISValidateSignatureAndCopyInfoWithProgress(CFStringRef path, CFDictionaryRef options, CFDictionaryRef *info, void *progressCallback);
+MISError MISValidationCopySignatureVersion(CFStringRef path, CFTypeRef signatureVersion);
+void MISEnumerateTrustedUPPs(void *enumBlock);
+void MISSetUPPTrust(CFStringRef upp, Boolean trust);
